16f72b0efaf36bfd40e2fa7050c0534162c640bf
sebastianpiresmolin <piresmolinsecure@gmail.com>
2025-04-08 16:03:00 +0200

    Install Nugets

--- Mini MES API/obj/Debug/net9.0/Mini MES API.AssemblyInfo.cs
+++ Mini MES API/obj/Debug/net9.0/Mini MES API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mini MES API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b3f91876cca5e683559990232b9385477aa74fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mini MES API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mini MES API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
